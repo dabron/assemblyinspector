e91360775d00dfe60a4a4195a14bf0f7c3b4a3ce
David Bronleewe <david@bronleewe.net>
2011-11-22 08:30:00 -0600

    Added icon and assembly information.

---
+++ AssemblyInspector/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Assembly Inspector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assembly Inspector")]
+[assembly: AssemblyCopyright("© 2011 David Bronleewe")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
